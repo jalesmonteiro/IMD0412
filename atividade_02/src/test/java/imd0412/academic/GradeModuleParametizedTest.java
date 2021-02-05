acb5a21bc0203aeceacc26f3fd73760477553628
Jales Monteiro <monteiro_iale@hotmail.com>
2021-02-05 15:26:19 -0300

    atividade 2 concluida

--- atividade_02/src/test/java/imd0412/academic/GradeModuleParametizedTest.java
+++ atividade_02/src/test/java/imd0412/academic/GradeModuleParametizedTest.java
@@ -19,30 +19,32 @@ public class GradeModuleParametizedTest {
 	@Parameters
 	public static Collection<Object[]> data() {
 		return Arrays.asList(new Object[][] {
-			{4.7f, 5.1f, 6.0f, 80, ApprovalStatus.APRN},
-			
-			{0.0f, 7.5f, 7.5f, 76, ApprovalStatus.REC},
-			{10.f, 2.5f, 2.5f, 76, ApprovalStatus.REC},
-			
+			{10.f, 10.f, 10.f, 75, ApprovalStatus.APR},
+			{7.0f, 7.0f, 7.1f, 75, ApprovalStatus.APR},
 			{7.0f, 7.0f, 7.0f, 75, ApprovalStatus.APR},
-			{7.0f, 7.0f, 6.9f, 75, ApprovalStatus.APRN},
 			
+			{7.0f, 7.0f, 6.9f, 75, ApprovalStatus.APRN},
+			{5.0f, 5.0f, 5.1f, 75, ApprovalStatus.APRN},
 			{5.0f, 5.0f, 5.0f, 75, ApprovalStatus.APRN},
+
 			{5.0f, 5.0f, 4.9f, 75, ApprovalStatus.REC},
 			{6.0f, 6.1f, 2.9f, 75, ApprovalStatus.REC},
-			
 			{3.0f, 3.0f, 3.0f, 75, ApprovalStatus.REC}, // falha -> REP
+
 			{3.0f, 3.0f, 2.9f, 75, ApprovalStatus.REP},
+			{0.0f, 0.0f, 0.0f, 75, ApprovalStatus.REP},
 			
+			{7.0f, 7.0f, 7.1f, 74, ApprovalStatus.REPF},
 			{7.0f, 7.0f, 7.0f, 74, ApprovalStatus.REPF},
 			{7.0f, 7.0f, 6.9f, 74, ApprovalStatus.REPF},
-			
+			{5.0f, 5.0f, 5.1f, 74, ApprovalStatus.REPF},
 			{5.0f, 5.0f, 5.0f, 74, ApprovalStatus.REPF},
 			{5.0f, 5.0f, 4.9f, 74, ApprovalStatus.REPF},
 			{6.0f, 6.1f, 2.9f, 74, ApprovalStatus.REPF},
-			
 			{3.0f, 3.0f, 3.0f, 74, ApprovalStatus.REPF}, // falha -> REMF
+			
 			{3.0f, 3.0f, 2.9f, 74, ApprovalStatus.REMF},
+			{0.0f, 0.0f, 0.0f, 74, ApprovalStatus.REMF},
 		});
 	}
 	
